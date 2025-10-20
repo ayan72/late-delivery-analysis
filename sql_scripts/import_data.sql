713047508e8cc203c6076c684a6ee008e5fa7974
Ayan <aabbas@ualberta.ca>
2025-10-20 16:12:28 +0530

    added all tables

--- sql_scripts/import_data.sql
+++ sql_scripts/import_data.sql
@@ -27,3 +27,39 @@ CREATE TABLE IF NOT EXISTS olist_customers (
   customer_city VARCHAR(50),
   customer_state VARCHAR(10)
 );
+
+CREATE TABLE IF NOT EXISTS olist_sellers (
+  seller_id TEXT PRIMARY KEY,
+  seller_zip_code_prefix VARCHAR(10),
+  seller_city TEXT,
+  seller_state VARCHAR(2)
+);
+
+-- SELECT * FROM olist_sellers LIMIT 10;
+
+CREATE TABLE IF NOT EXISTS olist_order_reviews (
+  review_id TEXT,
+  order_id TEXT,
+  review_score INT,
+  review_comment_title TEXT,
+  review_comment_message TEXT,
+  review_creation_date TIMESTAMP,
+  review_answer_timestamp TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS olist_products (
+  product_id TEXT PRIMARY KEY,
+  product_category_name TEXT,
+  product_name_length INT,
+  product_description_length INT,
+  product_photos_qty INT,
+  product_weight_g INT,
+  product_length_cm INT,
+  product_height_cm INT,
+  product_width_cm INT
+);
+
+CREATE TABLE IF NOT EXISTS product_category_name_translation (
+  product_category_name TEXT PRIMARY KEY,
+  product_category_name_english TEXT
+);
