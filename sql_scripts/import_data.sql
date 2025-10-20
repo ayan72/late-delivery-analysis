291bed97cc1ac1d6328ada3b5c97501ed45c8d24
Ayan <aabbas@ualberta.ca>
2025-10-20 15:59:30 +0530

    updated import_data file

--- sql_scripts/import_data.sql
+++ sql_scripts/import_data.sql
@@ -8,3 +8,22 @@ CREATE TABLE IF NOT EXISTS olist_orders (
   order_delivered_customer_date TIMESTAMP,   -- may be NULL for some rows
   order_estimated_delivery_date TIMESTAMP    
 );
+
+CREATE TABLE IF NOT EXISTS olist_order_items (
+  order_id TEXT,
+  order_item_id INT,
+  product_id TEXT,
+  seller_id TEXT,
+  shipping_limit_date TIMESTAMP,
+  price NUMERIC(12,2),
+  freight_value NUMERIC(12,2),
+  PRIMARY KEY (order_id, order_item_id)
+);
+
+CREATE TABLE IF NOT EXISTS olist_customers (
+  customer_id TEXT PRIMARY KEY,
+  customer_unique_id TEXT,
+  customer_zip_code_prefix INT,
+  customer_city VARCHAR(50),
+  customer_state VARCHAR(10)
+);
